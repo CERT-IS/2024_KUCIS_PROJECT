e910a7bb8acec09fedfc67ae8f82fe3d1bff8372
downfa11 <downfa11@naver.com>
2024-06-05 15:13:38 +0900

    init commit

---
+++ src/main/java/org/certis/siem/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package org.certis.siem.config;
+
+
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi elasticOpenApi(@Value("${springdoc.version}") String appVersion) {
+        String[] paths = { "/elastic-cluster/**" };
+        return GroupedOpenApi.builder().
+                group("elasticsearch")
+                .addOpenApiCustomizer(openApi -> openApi.info(new Info().title("Elasticsearch API").version(appVersion)))
+                .pathsToMatch(paths)
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi movieOpenApi(@Value("${springdoc.version}") String appVersion) {
+        String[] paths = { "/movie/**" };
+        return GroupedOpenApi.builder().
+                group("movies")
+                .addOpenApiCustomizer(openApi -> openApi.info(new Info().title("Movie API").version(appVersion)))
+                .pathsToMatch(paths)
+                .build();
+    }
+}
+
---
+++ src/main/java/org/certis/siem/controller/DashBoardController.java
@@ -0,0 +1,13 @@
+package org.certis.siem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashBoardController {
+
+    @GetMapping("")
+    public String main(){
+        return "index";
+    }
+}
