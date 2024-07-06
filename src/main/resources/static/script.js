90b92d768c6a581f3b0c1ffa734a040630050f61
downfa11 <downfa11@naver.com>
2024-07-07 08:17:20 +0900

    feat: modify script.js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,22 +1,139 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menu = document.querySelectorAll(".menu-link");
-    const menuToggle = document.querySelectorAll(".menu");
+document.addEventListener("DOMContentLoaded", function() {
+    const allDropdown = document.querySelectorAll('#sidebar .side-dropdown');
+    const sidebar = document.getElementById('sidebar');
 
+    allDropdown.forEach(item => {
+        const a = item.parentElement.querySelector('a:first-child');
+        a.addEventListener('click', function(e) {
+            e.preventDefault();
 
-    menuToggle.forEach((toggle) => {
-        toggle.addEventListener("click", function () {
-            const next = toggle.nextElementSibling;
-            next.classList.toggle("active");
+            if (!this.classList.contains('active')) {
+                allDropdown.forEach(i => {
+                    const aLink = i.parentElement.querySelector('a:first-child');
+
+                    aLink.classList.remove('active');
+                    i.classList.remove('show');
+                });
+            }
+
+            this.classList.toggle('active');
+            item.classList.toggle('show');
         });
     });
 
-    const sidebar = document.getElementById("sidebar");
-    const content = document.getElementById("content");
-    const toggleMenu = document.querySelector("[data-icon='menu']");
+    const toggleSidebar = document.querySelector('nav .material-symbols-outlined[data-icon="menu"]');
+    const allSideDivider = document.querySelectorAll('#sidebar .divider');
 
-    toggleMenu.addEventListener("click", function () {
-        sidebar.classList.toggle("active");
-        content.classList.toggle("active");
+    const updateSidebar = () => {
+        if (sidebar.classList.contains('hide')) {
+            allSideDivider.forEach(item => {
+                item.textContent = '-';
+            });
+
+            allDropdown.forEach(item => {
+                const a = item.parentElement.querySelector('a:first-child');
+                a.classList.remove('active');
+                item.classList.remove('show');
+            });
+        } else {
+            allSideDivider.forEach(item => {
+                item.textContent = item.dataset.text;
+            });
+        }
+    };
+
+    updateSidebar();
+
+    toggleSidebar.addEventListener('click', function() {
+        sidebar.classList.toggle('hide');
+        updateSidebar();
+    });
+
+    sidebar.addEventListener('mouseleave', function() {
+        if (this.classList.contains('hide')) {
+            allDropdown.forEach(item => {
+                const a = item.parentElement.querySelector('a:first-child');
+                a.classList.remove('active');
+                item.classList.remove('show');
+            });
+            allSideDivider.forEach(item => {
+                item.textContent = '-';
+            });
+        }
+    });
+
+    sidebar.addEventListener('mouseenter', function() {
+        if (this.classList.contains('hide')) {
+            allDropdown.forEach(item => {
+                const a = item.parentElement.querySelector('a:first-child');
+                a.classList.remove('active');
+                item.classList.remove('show');
+            });
+            allSideDivider.forEach(item => {
+                item.textContent = item.dataset.text;
+            });
+        }
+    });
+
+    const profile = document.querySelector('nav .profile');
+    const imgProfile = profile.querySelector('img');
+    const dropdownProfile = profile.querySelector('.profile-link');
+
+    imgProfile.addEventListener('click', function() {
+        dropdownProfile.classList.toggle('show');
+    });
+
+    const allMenu = document.querySelectorAll('main .content-data .head .menu');
+
+    allMenu.forEach(item => {
+        const icon = item.querySelector('.material-symbols-outlined[data-icon="more_horiz"]');
+        const menuLink = item.querySelector('.menu-link');
+
+        icon.addEventListener('click', function() {
+            menuLink.classList.toggle('show');
+        });
+    });
+
+    window.addEventListener('click', function(e) {
+        if (e.target !== imgProfile && e.target !== dropdownProfile) {
+            dropdownProfile.classList.remove('show');
+        }
+
+        allMenu.forEach(item => {
+            const icon = item.querySelector('.material-symbols-outlined[data-icon="more_horiz"]');
+            const menuLink = item.querySelector('.menu-link');
+
+            if (e.target !== icon && e.target !== menuLink) {
+                menuLink.classList.remove('show');
+            }
+        });
+    });
+
+    const allProgress = document.querySelectorAll('main .card .progress');
+
+    allProgress.forEach(item => {
+        item.style.setProperty('--value', item.dataset.value);
+    });
+
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
     });
 
     async function fetchSystemInfo() {
@@ -70,10 +187,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
         fetchEventStreams();
         fetchDangerousEvents();
-        
+
         setTimeout(pollData, 1000);
     }
 
     pollData();
-
 });
