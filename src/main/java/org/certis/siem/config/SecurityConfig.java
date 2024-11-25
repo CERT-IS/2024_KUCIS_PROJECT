b66b4d1224f925a99b563dbdad2ff446e1ea170d
downfa11 <downfa11@naver.com>
2024-11-26 00:36:41 +0900

    fix: security login error

--- src/main/java/org/certis/siem/config/SecurityConfig.java
+++ src/main/java/org/certis/siem/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package org.certis.siem.config;
 
+import java.net.URI;
+import org.opensearch.client.opensearch.nodes.Http;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -26,8 +32,18 @@ public class SecurityConfig {
                 )
                 .formLogin(login -> login
                         .loginPage("/login")
-                        .authenticationSuccessHandler((webFilterExchange, authentication) ->
-                                webFilterExchange.getExchange().getResponse().setComplete())
+                        .authenticationSuccessHandler((webFilterExchange, authentication) -> {
+                            webFilterExchange.getExchange()
+                                    .getResponse()
+                                    .setStatusCode(HttpStatus.FOUND);
+
+                            webFilterExchange.getExchange()
+                                    .getResponse()
+                                    .getHeaders()
+                                    .setLocation(URI.create("/"));
+
+                            return webFilterExchange.getExchange().getResponse().setComplete();
+                        })
                 )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
@@ -35,13 +51,24 @@ public class SecurityConfig {
 
     @Bean
     public ReactiveUserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+        UserDetails user = User.withUsername("user")
+                .password(passwordEncoder().encode("password"))
                 .roles("USER")
                 .build();
-        return (ReactiveUserDetailsService) new InMemoryUserDetailsManager(user);
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new MapReactiveUserDetailsService(user, admin);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
 
 
